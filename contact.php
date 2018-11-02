3beebbbb209b32e14ca60d012ef21c0d54d7cda3
Ethan Eisenhard <ethan@84g3-5cg82059vn.tb.tollbrothersinc.com>
2018-11-02 15:07:39 -0400

    portfolio

--- contact.php
+++ contact.php
@@ -1,127 +1,3 @@
-<?php
-// OPTIONS - PLEASE CONFIGURE THESE BEFORE USE!
-
-$yourEmail = "eeisen11@gmail.com"; // the email address you wish to receive these mails through
-$yourWebsite = "ethaneisenhard.com"; // the name of your website
-$thanksPage = ''; // URL to 'thanks for sending mail' page; leave empty to keep message on the same page 
-$maxPoints = 4; // max points a person can hit before it refuses to submit - recommend 4
-$requiredFields = "name,email,comments"; // names of the fields you'd like to be required as a minimum, separate each field with a comma
-
-
-// DO NOT EDIT BELOW HERE
-$error_msg = array();
-$result = null;
-
-$requiredFields = explode(",", $requiredFields);
-
-function clean($data) {
-	$data = trim(stripslashes(strip_tags($data)));
-	return $data;
-}
-function isBot() {
-	$bots = array("Indy", "Blaiz", "Java", "libwww-perl", "Python", "OutfoxBot", "User-Agent", "PycURL", "AlphaServer", "T8Abot", "Syntryx", "WinHttp", "WebBandit", "nicebot", "Teoma", "alexa", "froogle", "inktomi", "looksmart", "URL_Spider_SQL", "Firefly", "NationalDirectory", "Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot", "crawler", "www.galaxy.com", "Googlebot", "Scooter", "Slurp", "appie", "FAST", "WebBug", "Spade", "ZyBorg", "rabaz");
-
-	foreach ($bots as $bot)
-		if (stripos($_SERVER['HTTP_USER_AGENT'], $bot) !== false)
-			return true;
-
-	if (empty($_SERVER['HTTP_USER_AGENT']) || $_SERVER['HTTP_USER_AGENT'] == " ")
-		return true;
-	
-	return false;
-}
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	if (isBot() !== false)
-		$error_msg[] = "No bots please! UA reported as: ".$_SERVER['HTTP_USER_AGENT'];
-		
-	// lets check a few things - not enough to trigger an error on their own, but worth assigning a spam score.. 
-	// score quickly adds up therefore allowing genuine users with 'accidental' score through but cutting out real spam :)
-	$points = (int)0;
-	
-	$badwords = array("adult", "beastial", "bestial", "blowjob", "clit", "cum", "cunilingus", "cunillingus", "cunnilingus", "cunt", "ejaculate", "fag", "felatio", "fellatio", "fuck", "fuk", "fuks", "gangbang", "gangbanged", "gangbangs", "hotsex", "hardcode", "jism", "jiz", "orgasim", "orgasims", "orgasm", "orgasms", "phonesex", "phuk", "phuq", "pussies", "pussy", "spunk", "xxx", "viagra", "phentermine", "tramadol", "adipex", "advai", "alprazolam", "ambien", "ambian", "amoxicillin", "antivert", "blackjack", "backgammon", "texas", "holdem", "poker", "carisoprodol", "ciara", "ciprofloxacin", "debt", "dating", "porn", "link=", "voyeur", "content-type", "bcc:", "cc:", "document.cookie", "onclick", "onload", "javascript");
-
-	foreach ($badwords as $word)
-		if (
-			strpos(strtolower($_POST['comments']), $word) !== false || 
-			strpos(strtolower($_POST['name']), $word) !== false
-		)
-			$points += 2;
-	
-	if (strpos($_POST['comments'], "http://") !== false || strpos($_POST['comments'], "www.") !== false)
-		$points += 2;
-	if (isset($_POST['nojs']))
-		$points += 1;
-	if (preg_match("/(<.*>)/i", $_POST['comments']))
-		$points += 2;
-	if (strlen($_POST['name']) < 3)
-		$points += 1;
-	if (strlen($_POST['comments']) < 15 || strlen($_POST['comments']) > 1500)
-		$points += 2;
-	if (preg_match("/[bcdfghjklmnpqrstvwxyz]{7,}/i", $_POST['comments']))
-		$points += 1;
-	// end score assignments
-
-	if ( !empty( $requiredFields ) ) {
-		foreach($requiredFields as $field) {
-			trim($_POST[$field]);
-			
-			if (!isset($_POST[$field]) || empty($_POST[$field]) && array_pop($error_msg) != "Please fill in all the required fields and submit again.\r\n")
-				$error_msg[] = "Please fill in all the required fields and submit again.";
-		}
-	}
-
-	if (!empty($_POST['name']) && !preg_match("/^[a-zA-Z-'\s]*$/", stripslashes($_POST['name'])))
-		$error_msg[] = "The name field must not contain special characters.\r\n";
-	if (!empty($_POST['email']) && !preg_match('/^([a-z0-9])(([-a-z0-9._])*([a-z0-9]))*\@([a-z0-9])(([a-z0-9-])*([a-z0-9]))+' . '(\.([a-z0-9])([-a-z0-9_-])?([a-z0-9])+)+$/i', strtolower($_POST['email'])))
-		$error_msg[] = "That is not a valid e-mail address.\r\n";
-	
-	if ($error_msg == NULL && $points <= $maxPoints) {
-		$subject = "Automatic Form Email";
-		
-		$message = "You received this e-mail message through your website: \n\n";
-		foreach ($_POST as $key => $val) {
-			if (is_array($val)) {
-				foreach ($val as $subval) {
-					$message .= ucwords($key) . ": " . clean($subval) . "\r\n";
-				}
-			} else {
-				$message .= ucwords($key) . ": " . clean($val) . "\r\n";
-			}
-		}
-		$message .= "\r\n";
-		$message .= 'IP: '.$_SERVER['REMOTE_ADDR']."\r\n";
-		$message .= 'Browser: '.$_SERVER['HTTP_USER_AGENT']."\r\n";
-		$message .= 'Points: '.$points;
-
-		if (strstr($_SERVER['SERVER_SOFTWARE'], "Win")) {
-			$headers   = "From: $yourEmail\r\n";
-		} else {
-			$headers   = "From: $yourWebsite <$yourEmail>\r\n";	
-		}
-		$headers  .= "Reply-To: {$_POST['email']}\r\n";
-
-		if (mail($yourEmail,$subject,$message,$headers)) {
-			if (!empty($thanksPage)) {
-				header("Location: $thanksPage");
-				exit;
-			} else {
-				$result = 'Your mail was successfully sent.';
-				$disable = true;
-			}
-		} else {
-			$error_msg[] = 'Your mail could not be sent this time. ['.$points.']';
-		}
-	} else {
-		if (empty($error_msg))
-			$error_msg[] = 'Your mail looks too much like spam, and could not be sent this time. ['.$points.']';
-	}
-}
-function get_data($var) {
-	if (isset($_POST[$var]))
-		echo htmlspecialchars($_POST[$var]);
-}
-?>
 <!DOCTYPE html>
 <html>
         <head>
@@ -142,19 +18,19 @@ function get_data($var) {
 	
 <style>
 
-	p.error, p.success {
-			font-weight: bold;
-			padding: 10px;
-			border: 1px solid;
-		}
-		p.error {
-			background: #ffc0c0;
-			color: #900;
-		}
-		p.success {
-			background: #b3ff69;
-			color: #4fa000;
-		}
+p.error, p.success {
+		font-weight: bold;
+		padding: 10px;
+		border: 1px solid;
+}
+p.error {
+	background: #ffc0c0;
+	color: #900;
+}
+p.success {
+	background: #b3ff69;
+	color: #4fa000;
+}
 
 .hero-image {
  background: url("/WebsitesImages/wildfire.jpg");
@@ -197,7 +73,7 @@ function get_data($var) {
   color: white;
 }
 
-#footerload{
+#footerload {
 	margin-top: 25px;
 }
 

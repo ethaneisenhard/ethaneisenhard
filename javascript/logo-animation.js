4718046cce894fdd5fc40417b1695f53d3159d93
Ethan Eisenhard <eeisenhard@tollbrothers.com>
2020-12-06 17:47:30 -0500

    fix color hover

--- javascript/logo-animation.js
+++ javascript/logo-animation.js
@@ -1,54 +1,83 @@
-var colorArrays = ["#da4066", "#32c07a", "#32bee2", "#f5925d", "#293e6a", "#ffcf50"]
-
 var firstName = document.querySelector(".js-split-firstName");
 var splitFirstName = firstName.getAttribute("aria-label").split("");
-splitFirstName.forEach((letter) => {
+splitFirstName.forEach((letter, index) => {
+
+  var colorArrays = [
+    "#da4066",
+    "#32c07a",
+    "#32bee2",
+    "#f5925d",
+    "#293e6a",
+    "#ffcf50",
+  ];
   var createSpan = document.createElement("span");
   createSpan.innerHTML = letter;
   createSpan.classList.add("random-color");
-  if(letter === "E"){
+  var randomColor;
+  if(index === 0){
     colorArrays.pop()
+    randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+    createSpan.style.color = randomColor;
   }else{
-    colorArrays.push("#ffcf50")
+    randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+    createSpan.style.color = randomColor;
   }
-  var randomColor = colorArrays[Math.floor(Math.random()*colorArrays.length)];
-  createSpan.style.color = randomColor;
   firstName.appendChild(createSpan);
 });
 
 var lastName = document.querySelector(".js-split-lastName");
 var splitLastName = lastName.getAttribute("aria-label").split("");
-splitLastName.forEach((letter) => {
+splitLastName.forEach((letter, index) => {
+  var colorArrays = [
+    "#da4066",
+    "#32c07a",
+    "#32bee2",
+    "#f5925d",
+    "#293e6a",
+    "#ffcf50",
+  ];
   var createSpan = document.createElement("span");
   createSpan.innerHTML = letter;
   createSpan.classList.add("random-color");
-  if(letter === "E"){
+  if(index === 0){
     colorArrays.pop()
+    var randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+    createSpan.style.color = randomColor;
   }else{
-    colorArrays.push("#ffcf50")
+    var randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+    createSpan.style.color = randomColor;
   }
-  var randomColor = colorArrays[Math.floor(Math.random()*colorArrays.length)];
-  createSpan.style.color = randomColor;
   lastName.appendChild(createSpan);
 });
 
 function rgb2hex(rgb) {
   rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
   function hex(x) {
-      return ("0" + parseInt(x).toString(16)).slice(-2);
+    return ("0" + parseInt(x).toString(16)).slice(-2);
   }
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
-var changeColor = function(element, lastElement){
-    var randomColor = colorArrays[Math.floor(Math.random()*colorArrays.length)];
-    if(rgb2hex(element.target.style.color) !== randomColor){  
-        element.target.style.color = randomColor;
-    }
-}
+var changeColor = function (element) {
+  var colorArrays = [
+    "#da4066",
+    "#32c07a",
+    "#32bee2",
+    "#f5925d",
+    "#293e6a",
+    "#ffcf50",
+  ];
+  var randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+  if (rgb2hex(element.target.style.color) !== randomColor) {
+    element.target.style.color = randomColor;
+  } else {
+    var indexOfRandomColor = colorArrays.indexOf(randomColor);
+    colorArrays.splice(indexOfRandomColor, 1)
+    randomColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+    element.target.style.color = randomColor;
+  }
+};
 
-document.querySelectorAll(".random-color").forEach((element)=>{
-    element.addEventListener("mouseover", changeColor)
-})
-
-
+document.querySelectorAll(".random-color").forEach((element) => {
+  element.addEventListener("mouseover", changeColor);
+});

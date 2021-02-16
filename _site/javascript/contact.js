cad85b80dc5485c29cd305141d121ac16e6251ff
Ethan Eisenhard <eeisenhard@tollbrothers.com>
2021-02-15 22:51:01 -0500

    add eleventy

---
+++ _site/javascript/contact.js
@@ -0,0 +1,54 @@
+var contactBTN = document.querySelectorAll(".js-contact");
+var contactModalEl = document.querySelector(".js-contact-modal");
+var contactModalWrapperEl = document.querySelector(".js-contact-modal-wrapper");
+var contactModalBackgroundEl = document.querySelector(
+  ".js-contact-modal-background"
+);
+
+function toggleContact() {
+  contactModalEl.classList.toggle("is-hidden");
+  contactModalBackgroundEl.classList.toggle("is-hidden");
+
+  setTimeout(function () {
+    contactModalBackgroundEl.classList.toggle("is-active");
+  }, 20);
+
+  setTimeout(function () {
+    contactModalWrapperEl.classList.toggle("is-opacity-0");
+  }, 50);
+
+  setTimeout(function () {
+    contactModalWrapperEl.classList.toggle("is-open");
+  }, 60);
+}
+
+contactBTN.forEach((element) =>
+  element.addEventListener("click", toggleContact)
+);
+
+console.log(
+  `%c
+|    ____________________________________________________     |
+|   |                                                    |    |
+|   |                                                    |    |
+|   |   Site Created by Ethan Eisenhard                  |    |
+|   |   Built With                                       |    |
+|   |   Host: https://www.netlify.com/                   |    |
+|   |   HTML, CSS, JS                                    |    |
+|   |   No Frameworks                                    |    |
+|   |   https://www.svgbackgrounds.com/                  |    |
+|   |   All pictures taken by Ethan Eisenhard            |    |
+|   |   https://github.com/ethaneisenhard/ethaneisenhard |    |
+|   |                                                    |    |
+|   |                                                    |    |
+|   |                                                    |    |
+|   |____________________________________________________|    |
+          ___________________________________________
+      _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- -_
+      _-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.-_
+      _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. .-.-.-.-_
+      _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.-_
+      _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.-_
+      :-------------------------------------------------------------------------:`,
+  "font-family:monospace"
+);
---
+++ blog/post-one/never-again.md
@@ -0,0 +1,8 @@
+---
+title: Never Again
+blogDate: January 23, 2021
+---
+
+![Run away!](/Images/blog/never-again.jpg)
+
+Fool me once, shame on me, fool me twice, I start my own business
